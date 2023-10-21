addbb21a6f74e81786d803cca7b466a69e68c7b9
miield <aodasola95@gmail.com>
2023-10-21 19:28:54 +0100

    update the main and static

--- main.go
+++ main.go
@@ -6,22 +6,16 @@ import (
 	"net/http"
 )
 
-func main() {
-
-	// raw http.HandleFunc
-	// http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Welcome here...")
-	// })
-
-	// Handle the http.HandleFunc
-	http.HandleFunc("/hello", helloHandler)
-
-	fmt.Printf("Starting server at port 8080\n")
-	// start the server & listen for incoming requests
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		// error handling
-		log.Fatal(err)
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
 	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -39,4 +33,30 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Welcome here...")
+}
+
+func main() {
+
+	// raw http.HandleFunc, this is how the server handles requests
+	// http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Welcome here...")
+	// })
+
+	// serve the static file using the FileServer
+	fileServer := http.FileServer(http.Dir("./static"))
+	// handle the static file
+	http.Handle("/", fileServer)
+
+	// Handle the form path with http.HandleFunc
+	http.HandleFunc("/form", formHandler)
+
+	// Handle the hello path with http.HandleFunc
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	// start the server & listen for incoming requests
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		// error handling
+		log.Fatal(err)
+	}
 }
